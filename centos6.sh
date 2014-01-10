f6788aaaec6df11952c9c6ce427fcae20031afbe
Arie Wijayanto <me@kangarie.com>
2014-01-10 23:46:31 +0700

    Update centos6.sh

--- centos6.sh
+++ centos6.sh
@@ -62,8 +62,8 @@ cd
 wget https://github.com/KittyKatt/screenFetch/raw/master/screenfetch-dev
 mv screenfetch-dev /usr/bin/screenfetch
 chmod +x /usr/bin/screenfetch
-echo "clear" >> .profile
-echo "screenfetch" >> .profile
+echo "clear" >> .bash_profile
+echo "screenfetch" >> .bash_profile
 
 # install webserver
 cd
